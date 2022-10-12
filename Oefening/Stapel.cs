7596527e3b48af9493d68cdf107a2fe15b179590
VicHazewinkel <vic.hazewinkel@student.ehb.be>
2022-10-12 22:29:01 +0200

    classes

--- Oefening/Stapel.cs
+++ Oefening/Stapel.cs
@@ -6,9 +6,45 @@ using System.Threading.Tasks;
 
 namespace Oefening
 {
-    internal class Stapel
+    internal class Stapel<T>
     {
+        private List<T> Container = new List<T>();
 
+        public void Toevoegen(T item)
+        {
+            Container.Add(item);    
+        }
+
+        public T Verwijderen()
+        {
+            T last = Container.Last();
+            Container.Remove(last);
+            return last;
+        }
+
+        public void Leegmaken()
+        {
+            Container.Clear();
+        }
+
+        public string ToString()
+        {
+            return String.Join(" , ", Container.ToArray()); 
+        }
+
+        public bool IsAanwezig(T item)
+        {
+            return Container.Contains(item); 
+        }
+
+        public Stapel<T> Copy()
+        {
+            // ContainerCopy = Container.GetRange(0, Container.Count);
+            // return ContainerCopy
+
+            return (Stapel<T>)this.MemberwiseClone(); 
+
+        }
 
     }
 }
